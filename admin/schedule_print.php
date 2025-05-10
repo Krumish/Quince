ac7a0c31341607589ed699babfaa076bca6fc21f
Krumish <qaaknleyble@tip.edu.ph>
2025-05-10 14:12:27 +0800

    change button text

--- admin/schedule_print.php
+++ admin/schedule_print.php
@@ -38,7 +38,7 @@
     $pdf->AddPage();  
     $content = '';  
     $content .= '
-      	<h2 align="center">TechSoft IT Solutions</h2>
+      	<h2 align="center">JVJ Interior Supplies Trading Company</h2>
       	<h4 align="center">Employee Schedule</h4>
       	<table border="1" cellspacing="0" cellpadding="3">  
            <tr>  
